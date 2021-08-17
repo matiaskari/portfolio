81cb602e5ecf2a413603edf78127f9b28a453917
Matias Kari <46843190+matiaskari@users.noreply.github.com>
2021-08-17 20:05:06 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import backgroundImage from './components/img/DSC_0463.JPG'
+
+//Components
+import Header from './components/Header'
+import About from './components/About'
+import Projects from './components/Projects'
+import Education from './components/Education'
+
+const App = () => {
+    const styles = {
+        container: { //Creating the 2-column layout
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            alignItems: 'flex-start',
+            width: '100%',
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundColor: 'white', //If background image is unavailable
+            backgroundPosition: 'center',
+            backgroundSize: 'cover'
+        },
+        about: {
+            flex: '1', //Table of Contents width
+            width: '20%',
+            padding: '20px',
+            margin: '10px',
+
+            borderStyle: 'solid',
+            borderRadius: '10px',
+            borderWidth: '2px',
+            borderColor: '#417ec4',
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        },
+        content: {
+            flex: '3', //Rules width
+            width: '70%',
+            padding: '20px',
+            margin: '10px',
+            marginLeft: '0px',
+
+            borderStyle: 'solid',
+            borderRadius: '10px',
+            borderWidth: '2px',
+            borderColor: '#417ec4',
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        }
+    }
+
+    return (
+        <Router className="App">
+            <Header />
+            <div style={styles.container}>
+                <div style={styles.about}><About /></div>
+                <div style={styles.content}>
+                    <Route path="/projects"><Projects /></Route>
+                    <Route path="/education"><Education /></Route>
+                    <Route path="/"><Redirect to="/projects" /></Route>
+                </div>
+            </div>
+        </Router>
+    )
+}
+
+export default App;
---
+++ src/components/Projects.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import Project from './Project'
+import profilePic from './img/profile.jpg'
+import renegeoLogo from './img/renegeologo.png'
+import reaktorLogo from './img/reaktorlogo.png'
+
+const Projects = () => {
+    const descPortfolio = <p>This portfolio that you are currently viewing is made with React.js</p>
+    const descReaktorFall21 = <div>
+        <p>This website was made for the Reaktor Junior Developer pre-assignment for fall 2021.</p>
+        <p>
+            The solution dynamically imports a text file from Wizards of the Coast and creates an interactive rulebook with a clickable table of contents
+            for reading individual chapters. It also features a filtering search field.
+        </p>
+        <p>This was the first full project that I completed using React.js after learning it earlier the same year.</p>
+    </div>
+    const descReaktorSummer2021 = <div>
+        <p>This website was made for the Reaktor Junior Developer pre-assignment for summer 2021.</p>
+        <p>
+            The project featured working with two legacy APIs which can not be modified and involved numerous iterations of dealing with
+            unexpected issues that were put in place by Reaktor to test my problem-solving ability, such as:
+        </p>
+        <ul>
+            <li>Requirement to fetch data from 9 different URLs</li>
+            <li>Non-matching case in the IDs of items of each API</li>
+            <li>Fetching of some data randomly failing</li>
+            <li>All data being blocked by CORS</li>
+        </ul>
+    </div>
+    const descRenegeo = <p>This website was made as an university project with HTML, CSS and JavaScript and is the first website I ever made.</p>
+
+    const style = {
+        content: {
+            display: 'grid',
+            gridTemplateColumns: 'auto auto auto'
+        },
+        item: {
+            width: '33%'
+        }
+    }
+    return (
+        <div>
+            <h1>My projects</h1>
+            <div style={style.content}>
+                <Project
+                    style={style.item}
+                    name={'Portfolio'}
+                    year={'2021'}
+                    languages={'React.js, CSS'}
+                    shortDesc={descPortfolio}
+                    thumbnail={profilePic}
+                    codeLink={'#'}
+                    siteLink={'#'}
+                />
+                <Project
+                    style={style.item}
+                    name={'Magic: the Gathering Rulebook | Reaktor Junior Developer Assignment Fall 2021'}
+                    year={'2021'}
+                    languages={'React.js, Node.js'}
+                    shortDesc={descReaktorFall21}
+                    thumbnail={reaktorLogo}
+                    codeLink={'https://github.com/matiaskari/reaktorfall21'}
+                    siteLink={'https://reaktorfall21.herokuapp.com/'}
+                />
+                <Project
+                    style={style.item}
+                    name={'Inventory Management Tool | Reaktor Junior Developer Assignment Summer 2021'}
+                    year={'2020'}
+                    languages={'Javascript (vanilla), HTML, CSS'}
+                    shortDesc={descReaktorSummer2021}
+                    thumbnail={reaktorLogo}
+                    codeLink={'https://github.com/matiaskari/reaktorThing'}
+                    siteLink={'https://matiaskari.github.io/reaktorApp/reaktorApp.html'}
+                />
+                <Project
+                    style={style.item}
+                    name={'Geothermal Energy | School project'}
+                    year={'2019'}
+                    languages={'HTML, CSS, JavaScript (vanilla)'}
+                    shortDesc={descRenegeo}
+                    thumbnail={renegeoLogo}
+                    codeLink={'https://matiaskari.github.io/geohtml.html'}
+                    siteLink={'https://users.aalto.fi/~karim6/index.html'}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Projects
